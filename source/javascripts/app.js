a1217e5f65d978bcace2a06de37e1cec82ae8127
ejaypcanaria@gmail.com <ejaypcanaria@gmail.com>
2014-11-15 13:52:29 +0800

    Initial redesign - home page

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,7 +1,26 @@
 (function($, win, doc) {
 
-    $(doc).ready(function() {
+    var app = (function() {
+        var initUI = function() {
+            $('#wrapper')
+                .find('#logo')
+                    .velocity('transition.slideDownIn', {duration: 1000})
+                    .end()
+                .find('#main-content, #navigation')
+                    .velocity('transition.fadeIn', {delay: 400, duration: 1000})
+                    .end();
+        };
 
+        return {
+            init: function() {
+                initUI();
+            }
+        };
+    })();
+
+
+    $(doc).ready(function() {
+        app.init();
     });
 
 })(window.jQuery, window, document);
