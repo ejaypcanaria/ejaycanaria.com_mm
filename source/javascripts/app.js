fda7990fa56cd0c847e314da42329365793311a8
ejaypcanaria@gmail.com <ejaypcanaria@gmail.com>
2014-11-15 16:57:34 +0800

    Add new home page

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,23 +1,23 @@
 (function($, win, doc) {
 
     var app = (function() {
-        var initUI = function() {
+        var initLandingPage = function() {
             $('#wrapper')
                 .find('#logo')
                     .velocity('transition.slideDownIn', {duration: 1000})
                     .end()
-                .find('#main-content, #navigation')
+                .find('#content-area')
+                    .hide()
                     .velocity('transition.fadeIn', {delay: 400, duration: 1000})
                     .end();
         };
 
         return {
             init: function() {
-                initUI();
+                initLandingPage();
             }
         };
     })();
-
 
     $(doc).ready(function() {
         app.init();
