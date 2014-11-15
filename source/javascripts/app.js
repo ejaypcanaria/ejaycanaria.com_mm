ab489b288f6387c264a0b1fb168ce1f5c07662ee
ejaypcanaria@gmail.com <ejaypcanaria@gmail.com>
2014-11-15 11:46:04 +0800

    Clean up

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,13 +1,7 @@
 (function($, win, doc) {
 
     $(doc).ready(function() {
-        $('.main-nav ul li a').on('click', function() {
-            var target = this.dataset.target;
 
-            $('html, body').animate({scrollTop:  $(target).offset().top}, 800);
-
-            return false;
-        });
     });
 
 })(window.jQuery, window, document);
